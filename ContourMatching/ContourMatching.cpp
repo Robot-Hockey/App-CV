873fb50cd908719778e1ba5cfde5bc8043cff13c
Victor Moura <victor_cmoura@hotmail.com>
2019-10-13 19:07:03 -0300

    Adds reflection and more smoothness to the algorithm

--- ContourMatching/ContourMatching.cpp
+++ ContourMatching/ContourMatching.cpp
@@ -18,14 +18,6 @@ Mat src_gray, hsv, bin;
 int thresh = 100;
 RNG rng(12345);
 
-// int H_MIN = 83;
-// int H_MAX = 119;
-// int S_MIN = 126;
-// int S_MAX = 256;
-// int V_MIN = 0;
-// int V_MAX = 83;
-
-
 int H_MIN = 4;
 int H_MAX = 12;
 int S_MIN = 221;
@@ -34,8 +26,17 @@ int V_MIN = 71;
 int V_MAX = 228;
 
 Point previous = Point(0, 0);
-Point infinite;
-double m, b;
+Point infinite, reflection_point, infinite_reflection_point;
+double m, b, rm, rb;
+
+double diff_m = 0;
+
+double cx {0}, cy {0};
+double ix {0}, iy {0};
+
+bool reflects;
+
+float fps;
 
  // Convert to string
 #define SSTR( x ) static_cast< std::ostringstream & >( \
@@ -43,6 +44,10 @@ double m, b;
 
 double get_y(double m, double b, double x){
     return m*x + b;
+}
+
+double get_x(double m, double b, double y){
+    return (y-b)/m;
 }
 
 void thresh_callback(int, void* )
@@ -58,8 +63,8 @@ void thresh_callback(int, void* )
 
     /// Draw contours
 
-    double min_area = 100;
-    double max_area = 100;
+    double min_area = 0;
+    double max_area = 100000000;
 
     Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
     for( size_t i = 0; i< contours.size(); i++ ){
@@ -67,30 +72,89 @@ void thresh_callback(int, void* )
         if(area >= min_area){
             Rect br = boundingRect(contours[i]);
             
-            double cx = br.x+br.width/2; 
-            double cy = br.y+br.height/2;
+            cx = br.x+br.width/2; 
+            cy = br.y+br.height/2;
 
             Point curr = Point(cx, cy);
 
-            if(hypot(curr.x-previous.x, curr.y-previous.y) > 3){
+            double eps = 1.8;
+
+            double den = (infinite.x - curr.x);
+            if(fabs(den) < 1e-9){
+                den = 0.001;
+            }
+
+            double temp_m = (infinite.y - curr.y)/den;
+
+            diff_m = fabs(fabs(temp_m) - fabs(m));
+
+            // cout << temp_m << " " << m << " " << diff_m << endl;
+
+            if(diff_m > 0.008){
                 // Makes line algorithm (y = mx + b)
                 double den = (previous.x - curr.x);
                 if(fabs(den) < 1e-9){
                     den = 0.001;
                 }
-                m = (previous.y - curr.y)/den;
+
+                m = (previous.y - curr.y)/den;;
                 b = (curr.y - m*curr.x);
 
-                double infinite_x = 2*curr.x;
+                double infinite_x;
+
+                if(previous.x < curr.x){
+                    // Moving to the right of the screen
+                    infinite_x = 640;
+                }else{
+                    // Moving to the left of the screen
+                    infinite_x = 0;
+                }
+
+                
                 double infinite_y = get_y(m, b, infinite_x);
 
                 infinite = Point(infinite_x, infinite_y);
+                ix = infinite.x;
+                iy = infinite.y;
                 // ...
+
+                if(infinite.y <= 0 || infinite.y >= 480){
+                    reflects = true;
+
+                    double reflected_y;
+                    if(infinite.y <= 0){
+                        reflected_y = 0;
+                    }else{
+                        reflected_y = 480;
+                    }
+                    
+                    reflection_point = Point(
+                        get_x(m, b, reflected_y),
+                        reflected_y
+                    );
+
+                    rm = -m;
+                    rb = b = (reflection_point.y - rm*reflection_point.x);
+
+                    infinite_reflection_point = Point(
+                        infinite.x,
+                        get_y(rm, rb, infinite.x)
+                    );
+                    
+                }else{
+                    reflects = false;
+                }
 
                 previous = curr;
             }
 
-            line(drawing, curr, infinite, Scalar(0, 0, 256), 3);
+            line(drawing, curr, infinite, Scalar(0, 0, 256), 3); // First part of trajectory (Red)
+
+            if(reflects){
+                line(drawing, reflection_point, infinite_reflection_point, Scalar(256, 0, 0), 3); // Reflected trajectory (Blue)
+            }
+
+            line(drawing, Point(540, 0), Point(540, 480), Scalar(0, 256, 0), 2); // Attack line (Green)
 
             circle(drawing, curr, 1, Scalar(0, 0, 256), 5);
 
@@ -114,6 +178,9 @@ int main( int argc, char** argv )
     cap.open(deviceID + apiID);
 
     cap.set(CAP_PROP_FPS, 60);
+    // cap.set(CAP_PROP_FPS, 120);
+    // cap.set(CAP_PROP_FRAME_WIDTH,320);
+    // cap.set(CAP_PROP_FRAME_HEIGHT,240);
 
     while(1){
         double timer = (double)getTickCount();
@@ -135,8 +202,10 @@ int main( int argc, char** argv )
         const int max_thresh = 255;
         thresh_callback( 0, 0 );
         // Calculate Frames per second (FPS)
-        float fps = getTickFrequency() / ((double)getTickCount() - timer);
-        putText(src, "FPS : " + SSTR(int(fps)), Point(100,50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
+        fps = getTickFrequency() / ((double)getTickCount() - timer);
+        putText(src, "FPS: " + SSTR(int(fps)), Point(100,50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
+        putText(src, "POINT: " + SSTR(int(cx)) + ", " + SSTR(int(cy)), Point(100,100), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
+        putText(src, "DIFF M: " + SSTR(double(diff_m)), Point(100,200), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
         imshow( source_window, src );
 
 
